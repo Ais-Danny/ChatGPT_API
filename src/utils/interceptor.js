d3806843b9cafd3de52ff14d935cdd704b334495
Ais-Danny <2843508990@qq.com>
2023-05-03 14:42:30 +0800

    修复ip记录

--- src/utils/interceptor.js
+++ src/utils/interceptor.js
@@ -5,13 +5,14 @@ const jwt = require('jsonwebtoken');
  */
 function authInterceptor(req, res) {
     let token = req.headers['authorization']
+    let clientIP = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.ip;
     try {
         let result = jwt.verify(token, global.config.private_key)
         console.log('验证结果', result);
         return true;
     } catch (e) {
         res.send(new Result("校验失败,禁止访问", 403, false));
-        global.logger.warn(" 403,未授权的请求, ip "+req.ip+",Authorization:" + req.headers['authorization']);
+        global.logger.warn(" 403,未授权的请求, ip :"+clientIP+",Authorization:" + req.headers['authorization']);
         return false;
     }
 }
